93cdb4825adbfeccbbf00a9447f86f9550189db9
Dozay <edoziechidili@gmail.com>
2022-11-29 17:19:47 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
-const square = require('./5-square');
 
-class Square extends square {
-	charPrint (c) {
-		if (c === undefined) {
-			console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
-			} else {
-			console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
-			}
-	}
-}
-module.exports = Square;
+// Print function with custom characters to represent the Square
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
+  }
+};
