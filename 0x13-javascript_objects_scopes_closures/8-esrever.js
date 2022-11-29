f5508e8466ceb5bc6d614f16b045be1f45c9538a
Dozay <edoziechidili@gmail.com>
2022-11-29 17:22:26 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-	const arr = [];
-	for (let i = list.length - 1; i >= 0; i--) {
-		arr.push(list[i]);
-	}
-	return (arr);
+  const revList = [];
+
+  for (let i = list.length - 1; i >= 0; --i) {
+    revList.push(list[i]);
+  }
+
+  return revList;
 };
