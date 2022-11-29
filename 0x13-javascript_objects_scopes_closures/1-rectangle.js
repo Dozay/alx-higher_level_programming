1e3380f513841be1cbed0fb9612e796c7a4c70a5
Dozay <edoziechidili@gmail.com>
2022-11-29 17:12:36 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
-}
-module.exports = Rectangle;
+
+// Defines a Rectangle class with width and height attributes.
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
